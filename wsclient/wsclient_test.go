f1afc3c2236b4e19f23be47727bab462d47e0aa8
jesson <lkbtboy@gmail.com>
2021-03-31 19:50:36 +0800

    change test

--- wsclient/wsclient_test.go
+++ wsclient/wsclient_test.go
@@ -1,12 +1,15 @@
 package seelesdk
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
 
 	//"github.com/Seele-N/go-sdk/wsclient"
-	"github.com/Seele-N/go-sdk/types"
-	"github.com/stretchr/testify/assert"
+	//"github.com/Seele-N/go-sdk/types"
+	//"github.com/stretchr/testify/assert"
+	"github.com/Seele-N/go-sdk/common"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 )
 
 const (
@@ -14,12 +17,19 @@ const (
 )
 
 func TestQueryStatus(t *testing.T) {
-	config, err := types.NewClientConfig(rpcURL, "seelehubv1", "broadcastMode", "0.01seele", 100000, 0, "")
-	assert.Equal(t, nil, err)
+	common.SetConfig()
+	valAddress := "seelevaloper1cdz5t52jhzgtp7z8et6xlly5nvkfm99fx7kmtm"
+	addressBytes, _ := sdktypes.GetFromBech32(valAddress, common.ValidatorAddressPrefix)
+	address, _ := sdktypes.AccAddressFromHex(hex.EncodeToString(addressBytes))
+	fmt.Printf("address = %v", address.String())
+	/*
+		config, err := types.NewClientConfig(rpcURL, "seelehubv1", "broadcastMode", "0.01seele", 100000, 0, "")
+		assert.Equal(t, nil, err)
 
-	cli := NewWsClient(config)
+		cli := NewWsClient(config)
 
-	status, err := cli.Tendermint.QueryStatus()
-	assert.Equal(t, nil, err)
-	fmt.Printf("blockheight = %v", status.SyncInfo.LatestBlockHeight)
+		status, err := cli.Tendermint.QueryStatus()
+		assert.Equal(t, nil, err)
+		fmt.Printf("blockheight = %v", status.SyncInfo.LatestBlockHeight)
+	*/
 }
